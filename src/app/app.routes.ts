9ec0f13fdb145eaf75dc52dcb8107e3e470087c2
Omar Sotomayor <oscaromarfloressotomayor@gmail.com>
2025-07-27 14:51:37 -0600

    agrego rutas a origen de nuevo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { 
-    path: '', 
-    redirectTo: 'login', 
-    pathMatch: 'full' 
-  },
-  { 
-    path: 'login', 
-    loadComponent: () => import('./auth/pages/login/login/login.component').then(m => m.LoginComponent)
-  }
-];
+export const routes: Routes = [];
